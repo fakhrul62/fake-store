443b6a48a3f64b5c3f4557265899350842891520
Md. Fakhrul Alam Shuvo <ifakhrul23@gmail.com>
2024-06-07 01:35:09 +0600

    added

--- js/main.js
+++ js/main.js
@@ -1,17 +1,9 @@
-const url = 'https://api.worldnewsapi.com/search-news?text=earth+quake&language=en&earliest-publish-date=2024-04-01';
-const apiKey = '9c1896dfcfcd468586cc5bb850376cee';
+const getProductCatagory = async () =>{
+    const res = await fetch('https://fakestoreapi.com/products/categories');
+    const data = await res.json();
+    console.log(data);
+}
 
-fetch(url, {
-    method: 'GET',
-    headers: {
-        'x-api-key': apiKey
-    }
-})
-.then(response => {
-    if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json();
-})
-.then(data => console.log(data.news[0]))
-.catch(error => console.error('There was a problem with the fetch operation:', error));
+
+
+getProductCatagory()
